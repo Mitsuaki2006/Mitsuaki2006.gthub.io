bb08605aeca09f98a19ad63c6542548a8b013919
Mitsuaki2006 <109263366+Mitsuaki2006@users.noreply.github.com>
2022-07-18 09:05:28 +0900

    Add files via upload

---
+++ chara.js
@@ -0,0 +1,29 @@
+var img;
+
+function png1(){
+    img=document.getElementById("png5");
+    img.src="img/jett.png";
+    img=document.getElementById("png6");
+    img.src="img/reyna.png";
+}
+
+function png2(){
+    img=document.getElementById("png5");
+    img.src="img/skye.png";
+    img=document.getElementById("png6");
+    img.src="img/sova.png";
+}
+
+function png3(){
+    img=document.getElementById("png5");
+    img.src="img/brimstone.png";
+    img=document.getElementById("png6");
+    img.src="img/omen.png";
+}
+
+function png4(){
+    img=document.getElementById("png5");
+    img.src="img/killjoy.png";
+    img=document.getElementById("png6");
+    img.src="img/sage.png";
+}
